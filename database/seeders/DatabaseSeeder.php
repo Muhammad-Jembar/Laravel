c44ffb667278d4ed7655c1c4d2d07e2d91b0dc58
Muhammad-jembar <jembarrisky24@gmail.com>
2022-11-01 19:04:54 -0700

    update laravel

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -26,6 +26,12 @@ class DatabaseSeeder extends Seeder
             'password' => bcrypt('12345')
         ]);
 
+        User::create([
+            'name' => 'Hifal',
+            'email' => 'hifal24@gmail.com',
+            'password' => bcrypt('12345')
+        ]);
+
         Category::create([
             'name' => 'Web Programming',
             'slug' => 'web-programming'
@@ -64,6 +70,15 @@ class DatabaseSeeder extends Seeder
             'body' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Natus odio ipsum nobis, velit a esse dolorum accusantium autem corrupti dolor labore molestiae asperiores suscipit atque sequi ipsam. Quam, delectus nostrum doloremque cum dolore, quibusdam iure nobis quae itaque dolorum magnam architecto dicta obcaecati, unde reiciendis at quas animi eaque dignissimos explicabo rem impedit. Corporis magnam recusandae ad pariatur doloribus repellendus amet voluptatibus impedit cupiditate dolorem iusto voluptas deleniti ducimus tenetur veritatis ea facilis eum dolorum, commodi id sunt nemo. Mollitia asperiores fuga, veritatis reprehenderit inventore dolore aperiam aut natus quae minima sapiente molestiae voluptatibus iure accusamus explicabo numquam, totam corporis.',
             'category_id' => 2,
             'user_id' => 1      
+        ]);
+
+        Post::create([
+            'title' => 'Judul Ke Empat',
+            'slug' => 'judul-ke-empat',
+            'excerpt' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.',
+            'body' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Natus odio ipsum nobis, velit a esse dolorum accusantium autem corrupti dolor labore molestiae asperiores suscipit atque sequi ipsam. Quam, delectus nostrum doloremque cum dolore, quibusdam iure nobis quae itaque dolorum magnam architecto dicta obcaecati, unde reiciendis at quas animi eaque dignissimos explicabo rem impedit. Corporis magnam recusandae ad pariatur doloribus repellendus amet voluptatibus impedit cupiditate dolorem iusto voluptas deleniti ducimus tenetur veritatis ea facilis eum dolorum, commodi id sunt nemo. Mollitia asperiores fuga, veritatis reprehenderit inventore dolore aperiam aut natus quae minima sapiente molestiae voluptatibus iure accusamus explicabo numquam, totam corporis.',
+            'category_id' => 2,
+            'user_id' => 2    
         ]);
 
 
